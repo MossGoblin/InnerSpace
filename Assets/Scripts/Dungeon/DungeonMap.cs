439adce5648c9a6bf28e5f3f73f52abc24238859
Angel Slavchev <hugolabs@gmail.com>
2023-03-23 22:51:15 +0200

    Chunk placement fixed on Overlay

--- Assets/Scripts/Dungeon/DungeonMap.cs
+++ Assets/Scripts/Dungeon/DungeonMap.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class DungeonMap : MonoBehaviour
 {
     public Tile mapChunkPrefab;
     public Dungeon dungeon;
     private Dictionary<Address, Chunk> chunkList;
+    private double tileSize;
+
     void Awake()
     {
         chunkList = dungeon.GetChunkList();
         foreach (Address chunkAddress in chunkList.Keys)
         {
+            tileSize = 0.5;
             Tile newMapChunk = Instantiate(mapChunkPrefab, new Vector3(chunkAddress.addrP, chunkAddress.addrQ, 0), Quaternion.identity);
+            float offsetVertical = (float)(tileSize * chunkAddress.addrR * 3 / 2);
+            float offsetHorizontal = (float)(tileSize * (Math.Sqrt(3) * chunkAddress.addrQ + Math.Sqrt(3) / 2 * chunkAddress.addrR));
+
+            newMapChunk.transform.position = transform.position + new Vector3(offsetHorizontal, offsetVertical);
+
             newMapChunk.transform.SetParent(this.transform);
         }
     }
